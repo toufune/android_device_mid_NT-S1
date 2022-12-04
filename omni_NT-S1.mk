52bf67edbf64af619deda3bb5638af6712eb6607
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-12-04 14:24:25 +0900

    some changes

--- omni_NT-S1.mk
+++ omni_NT-S1.mk
@@ -19,6 +19,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from NT-S1 device
@@ -26,8 +27,8 @@ $(call inherit-product, device/mid/NT-S1/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/zImage-dtb:kernel
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := NT-S1
 PRODUCT_NAME := omni_NT-S1
